66ac83dabc8f0711963c8e20af22d5263913beaf
Jonaduro <durodola@hartford.edu>
2024-04-11 14:22:44 -0400

    nurse

--- src/Nurse.java
+++ src/Nurse.java
@@ -1,24 +1,44 @@
-class Nurse extends User{
+import java.util.ArrayList;
+import java.util.List;
 
-    public Nurse(String username, String password)
-    {
+public class Nurse extends User {
+
+    public Nurse(String username, String password) {
         super(username, password);
     }
 
-    public String orderTest(Patient patient)
-    {
-        return null;
+    public String orderTest(List<String> tests) {
+        
+        StringBuilder orderedTests = new StringBuilder("Ordered tests:");
+        for (String test : tests) {
+            orderedTests.append("\n- ").append(test);
+        }
+        return orderedTests.toString();
     }
 
-    public String addComment(Patient patient)
-    {
-        return null;
+    public String addComment(Patient patient, String comment) {
+        
+        return "Added comment for patient " + patient.getPatientName() + ": " + comment;
     }
 
-    @Override
-    public String toString()
-    {
-        return "\nUsername: " + this.getUsername()
-                + "\nPassword: " + this.getPassword();
+    public static void main(String[] args) {
+       
+        Nurse nurse = new Nurse("nurse123", "password");
+
+        // Use case: OrderTest
+        List<String> orderedTests = new ArrayList<>();
+        orderedTests.add("Red blood cell");
+        orderedTests.add("White blood cell");
+        System.out.println("Order Test:");
+        System.out.println(nurse.orderTest(orderedTests));
+
+        
+        Patient patient = new Patient("Hurtz", "1234");
+        String comment = "Patient reported severe chest pains.";
+        System.out.println("Add Comment:");
+        System.out.println(nurse.addComment(patient, comment));
     }
 }
+
+
+
